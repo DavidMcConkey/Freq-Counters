b7728fe3fb16f28af09c9524ee7751983459b5e0
david <davidmcconkey@outlook.com>
2023-08-03 16:08:38 -0400

    complete base problems

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,15 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+  if (!str1) return false;
+
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) {
+      i += 1;
+    }
+    if (i === str1.length) return true;
+    j += 1;
+  }
+  return false;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    if (nums[left] < 0 && nums[right] > 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left += 1;
+      right -= 1;
+    } else {
+      if (nums[left] > 0) left += 1;
+      else right -= 1;
+    }
+  }
+  return nums;
 }
